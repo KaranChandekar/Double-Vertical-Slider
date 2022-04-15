4d68a98a568420409f55c05aa90ec7293eda292e
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-15 19:39:49 +0530

    add conditionals

--- script.js
+++ script.js
@@ -14,4 +14,15 @@ downButton.addEventListener("click", () => changeSlide("down"));
 
 const changeSlide = (direction) => {
   const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
 };
